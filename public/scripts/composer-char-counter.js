1a8503ec71e286591a58cdd24bb5c9cde96a7b54
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-02-01 23:29:56 +0000

    implemnet function for create counter and counter style

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,16 @@
-$(document).ready(() => {});
+$(document).ready(() => {
+  const $tweetText = $("#tweet-text");
+  const $counter = $(".counter");
+
+  $tweetText.on("input", function () {
+    const maxmumLength = 140;
+    let textLength = $(this).val().length;
+    let remainingLength = maxmumLength - textLength;
+
+    // let counter = $(this).siblings(".new-tweet_bottom").children(".counter");
+    if (remainingLength < 0) {
+      $counter.addClass("btn_red");
+    }
+    $counter.text(maxmumLength - textLength);
+  });
+});
