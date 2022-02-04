f4f863cc59b4ccd6b4f22c40f894b2055d0c0cf5
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-02-04 00:59:14 +0000

    refactoring

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,19 +1,10 @@
 $(document).ready(() => {
-  //extract the only text part from the input//
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
-
   //fetch tweets form /tweets page//
   const loadTweets = () => {
-    $.ajax({
-      method: "GET",
-      url: "/tweets",
-    })
+    $.get("/tweets")
       .then((tweets) => {
         $("#tweet-container").empty();
+
         renderTweets(tweets);
       })
       .catch((error) => {
@@ -23,6 +14,56 @@ $(document).ready(() => {
 
   loadTweets();
 
+  // loops through tweets in the database and show //
+  const renderTweets = function (tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $("#tweet-container").prepend($tweet);
+    }
+  };
+  //hide the alert message first render//
+  const $alert = $(".alert");
+  $alert.hide();
+
+  //post new tweet //
+  $("#new-tweet_form").submit(function (e) {
+    //error message//
+
+    e.preventDefault();
+    const serializedData = $(this).serialize();
+    const textarea = $(this).children("#tweet-text").val();
+
+    if (textarea.length < 1) {
+      $(".alert")
+        .slideDown()
+        .children(".alert-message")
+        .text(`Input feild can't be empty`);
+    } else if (textarea.length > 140) {
+      $(".alert")
+        .show()
+        .children(".alert-message")
+        .text(`The tweet should be less than 140 words`);
+    } else {
+      $.post("/tweets", serializedData)
+        .then(() => {
+          $("#tweet-text").val("");
+          $(".counter").val("140");
+          loadTweets();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  });
+
+  //extract the only text part from the input//
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  //create HTML skeleton//
   const createTweetElement = (tweetData) => {
     const timePast = timeago.format(tweetData.created_at);
     const safeHTML = escape(tweetData.content.text);
@@ -58,64 +99,16 @@ $(document).ready(() => {
     return $tweet;
   };
 
-  const renderTweets = function (tweets) {
-    // loops through tweets
-    for (const tweet of tweets) {
-      // calls createTweetElement for each tweet
-      // takes return value and appends it to the tweets containe
-      const $tweet = createTweetElement(tweet);
-      $("#tweet-container").prepend($tweet);
-    }
-  };
-
-  // //hide the alert message first render//
-  const $alert = $(".alert");
-  $alert.hide();
-
   //form slide down and up//
   const $navArrow = $(".nav_arrow");
   const $newTweet = $(".new-tweet");
 
-  $navArrow.on("click", function () {
+  //nav button show and hide the input area//
+  $navArrow.click(function () {
     $newTweet
       .toggle()
       .children("#new-tweet_form")
       .children("#tweet-text")[0]
       .focus();
-  });
-
-  //post form with ajax//
-  const $form = $("#new-tweet_form");
-  $form.on("submit", function (e) {
-    e.preventDefault();
-    const serializedData = $(this).serialize();
-
-    const textarea = $(this).children("#tweet-text").val();
-
-    if (textarea.length < 1) {
-      $alert
-        .slideDown()
-        .children(".alert-message")
-        .text(`Input feild can't be empty`);
-    } else if (textarea.length > 140) {
-      $alert
-        .slideDown()
-        .children(".alert-message")
-        .text(`The tweet should be less than 140 words`);
-    } else {
-      $.ajax({
-        method: "POST",
-        url: "/tweets",
-        data: serializedData,
-      })
-        .then(() => {
-          console.log(`created new tweet`);
-          $("#tweet-text").val("");
-          loadTweets();
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
   });
 });
