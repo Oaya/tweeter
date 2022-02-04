f693c5c33cbe4cfe1e205090b23c9f37b458c68c
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-02-04 02:33:58 +0000

    refactoring

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,77 +1,36 @@
 $(document).ready(() => {
-  //fetch tweets form /tweets page//
-  const loadTweets = () => {
-    $.get("/tweets")
-      .then((tweets) => {
-        $("#tweet-container").empty();
-
-        renderTweets(tweets);
-      })
-      .catch((error) => {
-        console.log(`error: ${error}`);
-      });
-  };
-
+  $("#new-tweet_form").on("submit", onSubmit);
+  $(".nav_arrow").on("click", onClick);
   loadTweets();
+});
 
-  // loops through tweets in the database and show //
-  const renderTweets = function (tweets) {
-    for (const tweet of tweets) {
-      const $tweet = createTweetElement(tweet);
-      $("#tweet-container").prepend($tweet);
-    }
-  };
-
+//fetch tweets form /tweets page//
+const loadTweets = () => {
   //hide the alert message first render//
-  const $alert = $(".alert");
-  $alert.hide();
+  $(".alert").hide();
 
-  const checktweetLength = (textarea) => {
-    if (textarea.length < 1) {
-      return { message: `Input feild can't be empty` };
-    } else if (textarea.length > 140) {
-      return { message: `The tweet should be less than 140 words` }.text(``);
-    } else {
-      return { message: false };
-    }
-  };
+  $.get("/tweets")
+    .then((tweets) => {
+      renderTweets(tweets);
+    })
+    .catch((error) => {
+      console.log(`error: ${error}`);
+    });
+};
 
-  //post new tweet //
-  $("#new-tweet_form").submit(function (e) {
-    e.preventDefault();
-    const serializedData = $(this).serialize();
-    const textarea = $(this).children("#tweet-text").val();
+//extract the only text part from the input//
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
-    const error = checktweetLength(textarea);
+//create HTML skeleton//
+const createTweetElement = (tweetData) => {
+  const timePast = timeago.format(tweetData.created_at);
+  const safeHTML = escape(tweetData.content.text);
 
-    if (error.message) {
-      $alert.slideDown().children(".alert-message").text(error.message);
-    } else {
-      $.post("/tweets", serializedData)
-        .then(() => {
-          $("#tweet-text").val("");
-          $(".counter").val("140");
-          loadTweets();
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  });
-
-  //extract the only text part from the input//
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
-
-  //create HTML skeleton//
-  const createTweetElement = (tweetData) => {
-    const timePast = timeago.format(tweetData.created_at);
-    const safeHTML = escape(tweetData.content.text);
-
-    const $tweet = `
+  const $tweet = `
   <article class="tweet-list">
           <header class="tweet-header">
             <div class="tweet-header_left">
@@ -99,19 +58,57 @@ $(document).ready(() => {
         </article>
   `;
 
-    return $tweet;
-  };
+  return $tweet;
+};
 
-  //form slide down and up//
-  const $navArrow = $(".nav_arrow");
-  const $newTweet = $(".new-tweet");
+// loops through tweets in the database and show //
+const renderTweets = function (tweets) {
+  $("#tweet-container").empty();
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $("#tweet-container").prepend($tweet);
+  }
+};
 
-  //nav button show and hide the input area//
-  $navArrow.click(function () {
-    $newTweet
-      .toggle()
-      .children("#new-tweet_form")
-      .children("#tweet-text")[0]
-      .focus();
-  });
-});
+const checktweetLength = (textarea) => {
+  if (textarea.length < 1) {
+    return { message: `Input feild can't be empty` };
+  } else if (textarea.length > 140) {
+    return { message: `The tweet should be less than 140 words` }.text(``);
+  } else {
+    return { message: false };
+  }
+};
+
+//post new tweet //
+const onSubmit = function (e) {
+  e.preventDefault();
+
+  const serializedData = $(this).serialize();
+  const textarea = $(this).children("#tweet-text").val();
+
+  const error = checktweetLength(textarea);
+
+  if (error.message) {
+    $(".alert").slideDown().children(".alert-message").text(error.message);
+  } else {
+    $.post("/tweets", serializedData)
+      .then(() => {
+        $("#tweet-text").val("");
+        $(".counter").val("140");
+        loadTweets();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+};
+
+//nav button show and hide the input area//
+const onClick = function () {
+  $(".new-tweet")
+    .toggle()
+    .children("#new-tweet_form")
+    .children("#tweet-text")[0]
+    .focus();
+};
